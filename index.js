2066bda188098cc72b84882b3e6e8f348990f667
yulyazbrv <zubareva.yo@gmail.com>
2023-06-03 12:28:48 +0300

    feat: remove recursion

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const MoveTable = require("./Table/table");
 
 async function main() {
   const moves = process.argv.slice(2);
+
   if (moves.length < 3 || moves.length % 2 === 0) {
-    console.error("Error: Invalid number of moves.");
+    console.error('Error: Invalid number of moves.');
     return;
   }
 
   if (new Set(moves).size !== moves.length) {
-    console.error("Error: Duplicate moves found.");
+    console.error('Error: Duplicate moves found.');
     return;
   }
 
@@ -20,45 +21,47 @@ async function main() {
   const computerMove = moves[Math.floor(Math.random() * moves.length)];
   const key = KeyGenerator.generateKey(32);
   const hmac = HMAC.generateHmac(computerMove, key);
+
   console.log(`HMAC: ${hmac.toUpperCase()}`);
+
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
   });
 
-  const menu =
-    moves.map((move, index) => `${index + 1} - ${move}`).join("\n") +
-    "\n0 - Exit\n? - Help";
+  let choice = '';
 
-  const askForMove = () => {
-    rl.question(`${menu}\nEnter your move: `, (choice) => {
-      if (choice === "0") {
-        console.log("Bye!");
-        rl.close();
-        return;
-      } else if (choice === "?") {
-        const moveTable = new MoveTable(moves);
-        moveTable.printTable();
-        askForMove();
-        return;
-      }
+  while (choice !== '0') {
+    const menu = moves.map((move, index) => `${index + 1} - ${move}`).join('\n') + '\n0 - Exit\n? - Help';
 
-      const playerMove = moves[parseInt(choice, 10) - 1];
-      if (!playerMove) {
-        console.error("Error: Invalid choice. Please try again.");
-        askForMove();
-        return;
-      }
-
-      console.log(`Your move: ${playerMove}`);
-      console.log(`Computer move: ${computerMove}`);
-      console.log(gameRules.getResult(playerMove, computerMove));
-      console.log(`HMAC Key: ${key.toString("hex").toUpperCase()}`);
-      rl.close();
+    choice = await new Promise((resolve) => {
+      rl.question(`${menu}\nEnter your move: `, (input) => {
+        resolve(input.trim());
+      });
     });
-  };
 
-  askForMove();
+    if (choice === '0') {
+      console.log('Bye!');
+      break;
+    } else if (choice === '?') {
+      const moveTable = new MoveTable(moves);
+      moveTable.printTable();
+    } else {
+      const playerMove = moves[parseInt(choice, 10) - 1];
+
+      if (!playerMove) {
+        console.error('Error: Invalid choice. Please try again.');
+      } else {
+        console.log(`Your move: ${playerMove}`);
+        console.log(`Computer move: ${computerMove}`);
+        console.log(gameRules.getResult(playerMove, computerMove));
+        console.log(`HMAC Key: ${key.toString('hex').toUpperCase()}`);
+        break;
+      }
+    }
+  }
+
+  rl.close();
 }
 
 main();
