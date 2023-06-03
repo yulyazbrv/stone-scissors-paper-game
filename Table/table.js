69fb5a88a481037aceb1862a968b79cc992f0ef5
yulyazbrv <zubareva.yo@gmail.com>
2023-06-03 12:20:58 +0300

    feat: change class Table(use ACSI tables)

--- Table/table.js
+++ Table/table.js
@@ -1,10 +1,13 @@
-class Table {
+const Table = require('cli-table3');
+
+class MoveTable {
   constructor(moves) {
     this.moves = moves;
   }
 
   generateTable() {
-    const table = [[''].concat(this.moves)];
+    const head = [''].concat(this.moves);
+    const table = new Table({ head, style: { head: [], border: [] }, chars: { 'mid-mid': '+' } });
 
     for (const move1 of this.moves) {
       const row = [move1];
@@ -26,6 +29,11 @@ class Table {
 
     return table;
   }
+
+  printTable() {
+    const generatedTable = this.generateTable();
+    console.log(generatedTable.toString());
+  }
 }
 
-module.exports = Table;
+module.exports = MoveTable;
--- index.js
+++ index.js
@@ -1,62 +1,64 @@
-const readline = require('readline');
-const HMAC = require('./HMAC/hmac');
-const Rules = require('./Rules/rules');
-const Table = require('./Table/table');
-const KeyGenerator = require('./KeyGenerator/keyGenerator');
+const readline = require("readline");
+const HMAC = require("./HMAC/hmac");
+const Rules = require("./Rules/rules");
+const KeyGenerator = require("./KeyGenerator/keyGenerator");
+const MoveTable = require("./Table/table");
 
 async function main() {
   const moves = process.argv.slice(2);
-
   if (moves.length < 3 || moves.length % 2 === 0) {
-    console.error('Error: Invalid number of moves.');
+    console.error("Error: Invalid number of moves.");
     return;
   }
 
   if (new Set(moves).size !== moves.length) {
-    console.error('Error: Duplicate moves found.');
+    console.error("Error: Duplicate moves found.");
     return;
   }
-  
+
   const gameRules = new Rules(moves);
   const computerMove = moves[Math.floor(Math.random() * moves.length)];
   const key = KeyGenerator.generateKey(32);
   const hmac = HMAC.generateHmac(computerMove, key);
-
   console.log(`HMAC: ${hmac.toUpperCase()}`);
-
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
   });
 
-  const menu = moves.map((move, index) => `${index + 1} - ${move}`).join('\n') + '\n0 - Exit\n' + '? - Help';
+  const menu =
+    moves.map((move, index) => `${index + 1} - ${move}`).join("\n") +
+    "\n0 - Exit\n? - Help";
 
-  rl.question(`${menu}\nEnter your move: `, (choice) => {
-    rl.close();
+  const askForMove = () => {
+    rl.question(`${menu}\nEnter your move: `, (choice) => {
+      if (choice === "0") {
+        console.log("Bye!");
+        rl.close();
+        return;
+      } else if (choice === "?") {
+        const moveTable = new MoveTable(moves);
+        moveTable.printTable();
+        askForMove();
+        return;
+      }
 
-    if (choice === '0') {
-      console.log('Bye!');
-      return;
-    } else if (choice === '?') {
-      const moveTable = new Table(moves);
-      const table = moveTable.generateTable();
-      console.log('\n' + table.map(row => row.join('     ')).join('\n'));
-      return;
-    }
+      const playerMove = moves[parseInt(choice, 10) - 1];
+      if (!playerMove) {
+        console.error("Error: Invalid choice. Please try again.");
+        askForMove();
+        return;
+      }
 
-    const playerMove = moves[parseInt(choice, 10) - 1];
+      console.log(`Your move: ${playerMove}`);
+      console.log(`Computer move: ${computerMove}`);
+      console.log(gameRules.getResult(playerMove, computerMove));
+      console.log(`HMAC Key: ${key.toString("hex").toUpperCase()}`);
+      rl.close();
+    });
+  };
 
-    if (!playerMove) {
-      console.error('Error: Invalid choice. Please try again.');
-      main()
-      return;
-    }
-
-    console.log(`Your move: ${playerMove}`);
-    console.log(`Computer move: ${computerMove}`);
-    console.log(gameRules.getResult(playerMove, computerMove));
-    console.log(`HMAC Key: ${key.toString('hex').toUpperCase()}`);
-  });
+  askForMove();
 }
 
 main();
